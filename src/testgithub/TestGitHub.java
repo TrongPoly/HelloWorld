2b6f3d73493d7bf9402a6f84d0624be3ee1971a0
TrongPoly <trongnguyen32293@gmail.com>
2022-10-31 11:00:32 +0700

    add file

---
+++ src/testgithub/TestGitHub.java
@@ -0,0 +1,11 @@
+package testgithub;
+public class TestGitHub {
+    public static void main(String[] args) {
+       int i = 0;
+       while(i<6){
+           System.out.println("Trong");
+           i++;
+       }
+    }
+    
+}
