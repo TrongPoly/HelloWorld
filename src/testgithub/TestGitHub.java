2d0d66def2cb4207bc012586b2c4265639acc826
TrongPoly <103204477+TrongPoly@users.noreply.github.com>
2022-10-31 11:03:46 +0700

    Update TestGitHub.java
    thêm stt

--- src/testgithub/TestGitHub.java
+++ src/testgithub/TestGitHub.java
@@ -3,7 +3,7 @@ public class TestGitHub {
     public static void main(String[] args) {
        int i = 0;
        while(i<6){
-           System.out.println("Trong");
+           System.out.println("Trong"+i);
            i++;
        }
     }
