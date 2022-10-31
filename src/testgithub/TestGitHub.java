9ba94142010708962db346b1d89bdf71cb1b081a
TrongPoly <103204477+TrongPoly@users.noreply.github.com>
2022-10-31 11:06:10 +0700

    tăng số

--- src/testgithub/TestGitHub.java
+++ src/testgithub/TestGitHub.java
@@ -2,7 +2,7 @@ package testgithub;
 public class TestGitHub {
     public static void main(String[] args) {
        int i = 0;
-       while(i<6){
+       while(i<16){
            System.out.println("Trong"+i);
            i+=2;
        }
